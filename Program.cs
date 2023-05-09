00320bc5aa3723beb4204326f02e13d4bbfcf6f4
Anson Lee (1000103819) <LEES27@student.op.ac.nz>
2023-05-09 13:36:39 +1200

    creating new projet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_hub_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
