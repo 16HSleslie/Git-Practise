863760fafea0e2c34c9950cdffa694e6132a3768
Harry Stanley (1000070907) <STANHL1@student.op.ac.nz>
2023-05-09 13:44:07 +1200

    Hello

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Git_hub_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello i'm Harry");
         }
     }
 }
