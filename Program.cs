36d1b9d01ec69cf4524693a042750944c6183505
Harry S <stanhl1@student.op.ac.nz>
2023-05-09 20:23:17 +1200

    Added ability to enter name and respond with Hello

--- Program.cs
+++ Program.cs
@@ -11,6 +11,9 @@ namespace Git_hub_1
             Console.WriteLine("Hello I'm Liz");
             Console.WriteLine("hello im anson");
             Console.WriteLine("我是Ruel\n我爱篮球");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
         }
     }
 }
